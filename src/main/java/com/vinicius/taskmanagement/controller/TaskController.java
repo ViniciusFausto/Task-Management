d1fba77b22e57064ff09fc9b2371d7a289434811
Vinicius Gabriel Tronco Fausto <viniciusgtfausto@gmail.com>
2023-09-28 23:00:08 -0300

    Merge branch 'main' of D:\Vinic\Documents\Projetos\ProjetosJava\Task-Management with conflicts.

--- src/main/java/com/vinicius/taskmanagement/controller/TaskController.java
+++ src/main/java/com/vinicius/taskmanagement/controller/TaskController.java
@@ -1,30 +1,36 @@
 package com.vinicius.taskmanagement.controller;
 
 
-import com.vinicius.taskmanagement.application.usecaseimpl.TaskUseCaseImpl;
 import com.vinicius.taskmanagement.core.entity.Task;
 import com.vinicius.taskmanagement.core.usecase.TaskUseCase;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
+import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/task")
 public class TaskController {
 
-    @Autowired
-    TaskUseCase taskUseCaseImpl;
+    private final TaskUseCase taskUseCase;
+
     @PostMapping
     public ResponseEntity registerTask(@Valid @RequestBody Task data){
-            taskUseCaseImpl.save(data);
+            taskUseCase.save(data);
             return ResponseEntity.status(201).body(data);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getTaskById(@Valid @PathVariable Integer id){
+
+        Optional<Task> task = taskUseCase.findById(id);
+        if(task.isPresent()) {
+            return ResponseEntity.ok(task.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
