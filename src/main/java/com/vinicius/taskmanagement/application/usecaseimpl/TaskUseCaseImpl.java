9ccabf6bb791e96d0dc903478f9bf519489f8a63
Vinicius Gabriel Tronco Fausto <viniciusgtfausto@gmail.com>
2023-10-02 12:27:08 -0300

    feature: I've been done news endpoints to update/remove/get all tasks.

--- src/main/java/com/vinicius/taskmanagement/application/usecaseimpl/TaskUseCaseImpl.java
+++ src/main/java/com/vinicius/taskmanagement/application/usecaseimpl/TaskUseCaseImpl.java
@@ -3,9 +3,12 @@ package com.vinicius.taskmanagement.application.usecaseimpl;
 import com.vinicius.taskmanagement.core.entity.Task;
 import com.vinicius.taskmanagement.core.usecase.TaskUseCase;
 import com.vinicius.taskmanagement.infraestructure.repository.TaskRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,5 +25,31 @@ public class TaskUseCaseImpl implements TaskUseCase {
     @Override
     public Optional<Task> findById(Integer id) {
         return taskRepository.findById(id);
+    }
+    @Override
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Task update(Integer id, Task task) {
+        Task taskSaved = taskRepository.findById(id).orElseThrow(() -> new EmptyResultDataAccessException(1));
+        BeanUtils.copyProperties(task, taskSaved, "ID");
+        return taskRepository.save(taskSaved);
+    }
+
+    @Override
+    public void remove(Integer id) throws Exception {
+        taskExistValidation(id);
+        taskRepository.deleteById(id);
+    }
+
+    public void taskExistValidation(Integer id)throws Exception {
+        Optional
+                .ofNullable(id)
+                .orElseThrow(() -> new Exception() );
+
+         taskRepository.findById(id)
+                .orElseThrow( () -> new Exception());
     }
 }
--- src/main/java/com/vinicius/taskmanagement/controller/TaskController.java
+++ src/main/java/com/vinicius/taskmanagement/controller/TaskController.java
@@ -5,9 +5,11 @@ import com.vinicius.taskmanagement.core.entity.Task;
 import com.vinicius.taskmanagement.core.usecase.TaskUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,11 +25,32 @@ public class TaskController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@Valid @PathVariable Integer id){
+    public ResponseEntity<Task> getTaskById(@PathVariable Integer id){
         Optional<Task> task = taskUseCase.findById(id);
         if(task.isPresent()) {
             return ResponseEntity.ok(task.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/findAll")
+    public List<Task> getAll(){
+        return taskUseCase.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> update(@PathVariable Integer id, @Valid @RequestBody Task task) {
+        Task taskSaved = taskUseCase.update(id, task);
+        return ResponseEntity.ok(taskSaved);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity remove(@PathVariable("id") Integer id) {
+        try {
+            taskUseCase.remove(id);
+            return ResponseEntity.ok(null);
+        }catch (Exception ex) {
+            return ResponseEntity.noContent().build();
+        }
     }
 }
